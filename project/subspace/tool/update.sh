e146ada3ea9fe29ab11c576bab123d519f5ad6c0
TheGuild-ops <79193549+TheGuild-ops@users.noreply.github.com>
2022-10-05 18:52:27 +0300

    Update update.sh

--- project/subspace/tool/update.sh
+++ project/subspace/tool/update.sh
@@ -2,6 +2,7 @@ KEY=$(sudo docker run parity/subkey:latest  generate --network subspace_testnet 
 echo $KEY >> key.json
 WALLET_ADDRESS=$(cat key.json | jq .ss58Address | tail -n -1)
 echo { \"adress\" : "$WALLET_ADDRESS" } > keyLast.json
+echo Change. New adress is $(cat keyLast.json)
 PLOT_SIZE=100G
 docker-compose down
 wget https://raw.githubusercontent.com/TheGuild-ops/tool/main/project/subspace/docker-compose.yaml -O install && bash install moniker $WALLET_ADDRESS $PLOT_SIZE 2a-2022-sep-10 && rm install
