9de0b56163bef8f358cc531224cd5cf95da2a18d
TheGuild-ops <79193549+TheGuild-ops@users.noreply.github.com>
2022-10-15 19:06:53 +0300

    Update cheack.js

--- project/subspace/tool/cheack.js
+++ project/subspace/tool/cheack.js
@@ -1,4 +1,6 @@
 'use strict';
+
+const num = process.argv[2];
 const { exec } = require('child_process');
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const wsProvider = new WsProvider('wss://subspace-gemini-2a-rpc.dwellir.com/');
@@ -6,13 +8,14 @@ const fs = require('fs');
 
 (async () => {
   try {
-    const rawdata = fs.readFileSync('/root/node/subspace/keyLast.json');
+    const rawdata = fs.readFileSync(`/root/node/subspace/${num}/keyLast.json`);
     const adress = JSON.parse(rawdata).adress;
     const api = await ApiPromise.create({ provider: wsProvider });
     const { nonce, data: balance } = await api.query.system.account(adress);
+    console.log(parseInt(balance.free));
     if (parseInt(balance.free) > 500000000000000000) {
       console.log('work');
-      const yourscript = exec('wget https://raw.githubusercontent.com/TheGuild-ops/tool/main/project/subspace/tool/update.sh -O update.sh; bash update.sh', (error, stdout, stderr) => {
+      const yourscript = exec(`wget https://raw.githubusercontent.com/TheGuild-ops/tool/main/project/subspace/tool/update.sh -O update.sh; bash update.sh ${num}`, (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
         if (error !== null) {
