b960e214d25d1a4d04a55c4ec88b70fa267c5454
TheGuild-ops <79193549+TheGuild-ops@users.noreply.github.com>
2022-10-17 10:26:05 +0300

    Update install.sh

--- project/subspace/new/install.sh
+++ project/subspace/new/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+PNAME=subspace
+PATH=$HOME/node/$PNAME
 
-while getopts n:p:a:c: flag
+while getopts n:p:a:c:v: flag
 do
     case "${flag}" in
         n) VALIDATOR_NAME=${OPTARG};;
         p) SUBSPACE_PLOT_SIZE=${OPTARG};;
         a) SUBSPACE_REWARD_ADRESS=${OPTARG};;
         c) COUNT=${OPTARG};;
+        v) VERSION=${OPTARG};;
     esac
 done
 
@@ -32,8 +35,16 @@ then
 fi
 apt update && apt update -y
 cd
-mkdir $HOME/node/subspace -p
-cd $HOME/node/subspace
+mkdir $PATH -p
+cd $PATH
+wget https://raw.githubusercontent.com/TheGuild-ops/tool/main/project/subspace/new/cheack.js -O cheack.js
+wget https://raw.githubusercontent.com/TheGuild-ops/tool/main/project/subspace/new/rebuild.sh -O rebuild.sh
+
+docker-compose down
+##node $PATH/cheack.js
+bash $PATH/rebuild.sh COUNT VERSION VALIDATOR_NAME SUBSPACE_PLOT_SIZE
+##docker-compose up -d
+
 
 echo "Validator name: $VALIDATOR_NAME";
 echo "Plot Size: $SUBSPACE_PLOT_SIZE";
