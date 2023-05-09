ee17695dc269000a0685ce3aa45418e0aa8952be
TheGuild-ops <79193549+TheGuild-ops@users.noreply.github.com>
2023-05-09 22:14:46 +0300

    Update roll_watcher.sh

--- install/massa/roll_watcher.sh
+++ install/massa/roll_watcher.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 cd $HOME/massa/massa-client/
 source /root/.profile
@@ -10,13 +9,17 @@ handle() {
   local wallet_address=$(jq -r "[.[]] | .[0].address_info.address // empty" <<<"$wallet_info")
   local candidate_rolls=$(jq -r "[.[]] | .[-1].address_info.candidate_rolls" <<<"$wallet_info")
   local balance=$(jq -r "[.[]] | .[-1].address_info.final_sequential_balance" <<<"$wallet_info")
+
   local roll_count=$(bc -l <<<"$balance/100")
 echo $wallet_info
 echo $wallet_address
 echo $candidate_rolls
 echo $balance
 echo $roll_count
-echo $PAS
+  if [ -z $ch ]; then
+        echo Go
+        ./massa-client -p $PAS node_start_staking $wallet_address
+  fi
   if [ -z "$wallet_address" ]; then
     echo "😔😔😔 Wallet not found."
     echo "❗❗❗ Create new wallet with $HOME/massa/massa-client/massa-client wallet_generate_private_key"
